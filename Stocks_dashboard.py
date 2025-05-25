258bfae813689e74d9437a751c0bf51424ca6af5
6NOD <6nod2024@gmail.com>
2025-05-25 13:19:50 +0400

    Update Stocks_dashboard.py

--- Stocks_dashboard.py
+++ Stocks_dashboard.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.graph_objects as go
 
 # Constants
-API_KEY = st.secrets.toml["API_KEY"]
+API_KEY = st.secrets["API_KEY"]
 BASE_URL = "https://api.twelvedata.com"
 
 # Top 10 US Stocks, Index Funds, ETFs
